feff1864cc0bb559409db7d142bfeeeb70f2a73e
nandiniAchugatla <155294588+nandiniAchugatla@users.noreply.github.com>
2024-04-10 17:51:32 +0530

    Jest Test And Coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'spreezy-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('spreezy-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(1).toBe(1)  });
-});
+  let fixture :  AppComponent;
+  beforeEach(()=>{
+    fixture=new AppComponent();
+  })
+  it('should have title spreezy-frontend',()=>{
+    expect(fixture.title).toEqual('spreezy-frontend');
+  })
+})
