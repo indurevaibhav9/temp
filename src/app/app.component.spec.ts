895776a9b9f1197e48e7411cd27572f8a7592363
nandiniAchugatla <155294588+nandiniAchugatla@users.noreply.github.com>
2024-04-12 14:11:09 +0530

    Basic Changes Regarding Format  Done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let fixture :  AppComponent;
-  beforeEach(()=>{
-    fixture=new AppComponent();
-  })
-  it('should have title spreezy-frontend',()=>{
-    expect(fixture.title).toEqual('spreezy-frontend');
-  })
-})
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'spreezy-frontend'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('spreezy-frontend');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('spreezy-frontend app is running!');
+  });
+});
