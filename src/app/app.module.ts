afc40f6b5ea9495c4b1eeef18c050ce09380d59a
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-09-12 01:45:20 +0530

    Applied changes from Feature/Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { AppComponent } from "./app.component";
 import { LoginModule } from "./screens/login/login.module";
 import { LogoutModule } from "./screens/logout/logout.module";
 import { GlobalErrorHandlerService } from "./services/global-error-handler.service";
-import { BusinessHomeModule } from "./screens/business-home/business-home.module";
-import { ConsumerHomeModule } from "./screens/consumer-home/consumer-home.module";
+<<<<<<< HEAD
 import { ReactiveFormsModule } from "@angular/forms";
 import { RegisterModule } from "./screens/register/register.module";
 import { SharedModuleModule } from "./shared/shared-module/shared.module";
+=======
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+>>>>>>> 5f5312f (Applied changes from Feature/Navbar)
 @NgModule({
   declarations: [
     AppComponent
