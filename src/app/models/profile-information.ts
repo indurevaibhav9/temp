f945e20e8e32e932beb013c7075ffe6c5457b1ac
Nikhil Gaikwad <gaikwadnikhil10012003@gmail.com>
2024-10-09 12:10:07 +0530

    done with basic structuring

--- src/app/models/profile-information.ts
+++
@@ -1,9 +0,0 @@
-export class ProfileInformation {
-    name: string='';
-    username: string='';
-    emailID: string='';
-    contactNumber: number;
-    gender:string='';
-    aadharcardNumber:number;
-    pancardNumber:string='';
-}
