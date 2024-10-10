196f46310a413c577269c0ab07e0d5fd89680573
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-10-10 14:33:59 +0530

    changes after pulling from develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,10 @@ import { AppComponent } from "./app.component";
 import { LoginModule } from "./screens/login/login.module";
 import { LogoutModule } from "./screens/logout/logout.module";
 import { GlobalErrorHandlerService } from "./services/global-error-handler.service";
+import { BusinessHomeModule } from "./screens/business-home/business-home.module";
+import { ConsumerHomeModule } from "./screens/consumer-home/consumer-home.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RegisterModule } from "./screens/register/register.module";
-import { SharedModuleModule } from "./shared/shared-module/shared.module";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BusinessHomeModule } from "./screens/business-home/business-home.module";
-import { AddPostModule } from "./screens/add-post/add-post.module";
-import { NotificationScreenModule } from "./screens/notification-screen/notification-screen.module";
-import { ProfileScreenModule } from "./screens/profile-screen/profile-screen.module";
-import { ConsumerHomeModule } from "./screens/consumer-home/consumer-home.module";
-import { SearchModule } from "./screens/search/search.module";
 
 @NgModule({
   declarations: [
@@ -32,16 +26,10 @@ import { SearchModule } from "./screens/search/search.module";
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
     LoginModule,
-    SharedModuleModule,
+    BusinessHomeModule,
+    ConsumerHomeModule,
     ReactiveFormsModule,
     RegisterModule,
-    FontAwesomeModule,
-    AddPostModule,
-    NotificationScreenModule,
-    ProfileScreenModule,
-    SearchModule,
-    ConsumerHomeModule,
-    BusinessHomeModule
   ],
   providers: [
     {
