316c9e7ccb2d5f4d8a6580339f6e190c6604a478
madhav256 <maddynade7@gmail.com>
2024-10-22 11:33:33 +0530

    commented search's route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,10 +28,6 @@ const routes: Routes = [
     loadChildren: () => import("./screens/consumer-home/consumer-home.module").then(m => m.ConsumerHomeModule),
     // canActivate: [customerGuard]
   },
-  { 
-    path: 'search',
-    loadChildren: () => import('./screens/search/search.module').then(m => m.SearchModule)
-  },
   { path: "**", redirectTo: "consumer-home" }
 ];
 
