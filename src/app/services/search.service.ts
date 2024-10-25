9bf99c0e9cc532f6f8ac70f781065383fb7c7d63
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-10-25 09:36:16 +0530

    pulled changes from feature/search

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,39 +1,62 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
+import { catchError, map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { UserProfileDTO } from '../models/UserProfileDTO';
+
+export interface BusinessWithImageUrl extends UserProfileDTO {
+  imageUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-
-  private businessesUrl = 'https://dummyjson.com/c/2441-5cf8-4c84-8430';
+  private businessesUrl = 'http://localhost:8762/user/search';
   private imageUrl = 'http://images.spreezy.in';
+  
+  private searchSubject = new Subject<string>();
+  private businessesSubject = new BehaviorSubject<BusinessWithImageUrl[]>([]);
+  
+  businesses$ = this.businessesSubject.asObservable();
 
-  constructor(private http: HttpClient) { 
-    console.log('SearchService initialized');
+  constructor(private http: HttpClient) {
+    this.initSearchSubscription(); 
   }
-  
-  getBusinesses(searchQuery: string): Observable<{ name: string, username: string, profilePicture: string }[]> {
-    const url = `${this.businessesUrl}/${searchQuery}`;
-    console.log('Making GET request to:', url);
-    
-    const headers = new HttpHeaders({
-      'ngrok-skip-browser-warning': 'true'
+
+  private initSearchSubscription(): void {
+    this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(query => this.fetchBusinesses(query))
+    ).subscribe(businesses => {
+      this.businessesSubject.next(businesses);
     });
-  
-    return this.http.get<{ name: string, username: string, profilePicture: string }[]>(url, { headers })
-      .pipe(
-        tap(response => console.log('API response:', response)),
-        catchError(error => {
-          console.error('Error fetching businesses:', error);
-          throw error;
-        })
-      );
+  }
+
+  search(query: string): void {
+    this.searchSubject.next(query);
+  }
+
+  clearSearch(): void {
+    this.businessesSubject.next([]);
+  }
+
+  private fetchBusinesses(searchQuery: string): Observable<BusinessWithImageUrl[]> {
+    const url = `${this.businessesUrl}/${searchQuery}`;
+    
+    return this.http.get<UserProfileDTO[]>(url).pipe(
+      map(response => response.map(business => ({
+        ...business,
+        imageUrl: this.getImageUrl(business.profilePicture)
+      }))),
+      catchError(error => {
+        return of([]);
+      })
+    );
   }
 
   getImageUrl(profilePicture: string): string {
-    return `${this.imageUrl}/${profilePicture}`;
+    return profilePicture ? `${this.imageUrl}/${profilePicture}` : 'assets/logo.png';
   }
 }
